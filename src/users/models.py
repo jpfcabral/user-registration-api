d2f5a4355ccd2c36d5737f8f9bc415d751230fca
jpfcabral <jpfcabral@gmail.com>
2022-12-20 14:51:49 -0300

    adding user module

--- src/users/models.py
+++ src/users/models.py
@@ -6,11 +6,11 @@ class User(BaseModel): # pylint: disable=R0903:too-few-public-methods
     name: str
     email: str
     password: str
-    checked: bool = False
 
 
 class UserDB(User):
     id: Optional[int]
+    checked: bool = False
 
 
     def __rep__(self):
