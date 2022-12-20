d59376185f295da29a8b6cf4a36bf94ba0d2f58e
jpfcabral <jpfcabral@gmail.com>
2022-12-20 11:04:59 -0300

    creating db connection

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+class Settings: # pylint: disable=R0903:too-few-public-methods
+    DB_USER = os.getenv('DB_USER', 'postgres')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = os.getenv('DB_PORT', '5432')
+    DB_NAME = os.getenv('DB_NAME', 'registration')
+
+settings = Settings()
---
+++ src/infra/database.py
@@ -0,0 +1,52 @@
+import psycopg2
+from psycopg2 import Error
+
+from src.config import settings
+
+class DBConnectionHandler:
+
+    def __init__(self) -> None:
+        self.connection = None
+        self.cursor = None
+
+    def __create_engine(self):
+        return psycopg2.connect(
+            user=settings.DB_USER,
+            password=settings.DB_PASSWORD,
+            host=settings.DB_HOST,
+            port=settings.DB_PORT,
+            database=settings.DB_NAME
+        )
+
+    def create_db_and_tables(self):
+        try:
+            self.connection = self.__create_engine()
+            self.cursor = self.connection.cursor()
+
+            create_table_query = '''CREATE TABLE users
+            (
+                ID INT PRIMARY KEY NOT NULL,
+                NAME TEXT NOT NULL,
+                EMAIL TEXT NOT NULL,
+                PASSWORD TEXT NOT NULL,
+                CHECKED BOOL NOT NULL
+            )
+            '''
+
+            self.cursor.execute(create_table_query)
+            self.connection.commit()
+        except (Exception, Error) as error: # pylint: disable=W0703:broad-except
+            print("Error while connecting to PostgreSQL", error)
+        finally:
+            if self.connection:
+                self.cursor.close()
+                self.connection.close()
+
+    def __enter__(self):
+        self.connection = self.__create_engine()
+        self.cursor = self.connection.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cursor.close()
+        self.connection.close()
