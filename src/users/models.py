6968aad085e323918ecd550e9c0df535afe16c4a
jpfcabral <jpfcabral@gmail.com>
2022-12-20 13:41:05 -0300

    update model schema

--- src/users/models.py
+++ src/users/models.py
@@ -1,17 +1,17 @@
 from typing import Optional
 from pydantic import BaseModel # pylint: disable=E0611:no-name-in-module
 
-class User(BaseModel):
-    id: Optional[int]
+
+class User(BaseModel): # pylint: disable=R0903:too-few-public-methods
     name: str
     email: str
     password: str
     checked: bool = False
 
+
+class UserDB(User):
+    id: Optional[int]
+
+
     def __rep__(self):
         return f"User [name={self.name}, email={self.email}, checked={self.checked}]"
-
-class UserCreate: # pylint: disable=R0903:too-few-public-methods
-    name: str
-    email: str
-    password: str
