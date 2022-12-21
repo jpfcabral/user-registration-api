d8712a5d987fa9471d29e1400f442614781ea0dd
jpfcabral <jpfcabral@gmail.com>
2022-12-21 10:03:50 -0300

    fitting user response

--- src/users/controller.py
+++ src/users/controller.py
@@ -1,7 +1,7 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
-from src.users.models import User
+from src.users.models import User, UserRead
 from src.users.services import UserService
 
 router = InferringRouter(tags=['Users'])
@@ -11,10 +11,10 @@ class UserController:
     def __init__(self) -> None:
         self.__user_service = UserService()
 
-    @router.post('/')
+    @router.post('/', response_model=UserRead)
     def create_user(self, user: User):
-        self.__user_service.create_user_db(user)
+        return self.__user_service.create_user_db(user)
 
-    @router.get('/')
+    @router.get('/', response_model=List[UserRead]|UserRead) # pylint: disable=E1131:unsupported-binary-operation
     def get_users(self, user_id: Optional[int] = None):
         return self.__user_service.get_user_from_db(user_id)
--- src/users/models.py
+++ src/users/models.py
@@ -2,16 +2,27 @@ from typing import Optional
 from pydantic import BaseModel # pylint: disable=E0611:no-name-in-module
 
 
-class User(BaseModel): # pylint: disable=R0903:too-few-public-methods
+class User(BaseModel):
     name: str
     email: str
     password: str
 
+    def __rep__(self):
+        return f"User [name={self.name}, email={self.email}"
 
 class UserDB(User):
     id: Optional[int]
     checked: bool = False
 
+    def __rep__(self):
+        return f"User [name={self.name}, email={self.email}, checked={self.checked}]"
+
+
+class UserRead(BaseModel):
+    id: int
+    name: str
+    email: str
+    checked: bool
 
     def __rep__(self):
         return f"User [name={self.name}, email={self.email}, checked={self.checked}]"
