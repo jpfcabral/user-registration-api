e091105435a4c74bbf8bf637bd94df253894bc01
jpfcabral <jpfcabral@gmail.com>
2022-12-22 11:22:48 -0300

    deleting unused service

--- src/users/services.py
+++ src/users/services.py
@@ -1,13 +1,9 @@
-from src.users.models import User, UserDB
+from src.users.models import User
 from src.users.repository import UserRepository
 
 class UserService:
     def __init__(self, user_repository: UserRepository = UserRepository()) -> None:
         self.__user_repository = user_repository
-
-    def create_user_db(self, user: User):
-        user_db = UserDB.parse_obj(user)
-        return self.__user_repository.insert(user_db)
 
     def get_user_from_db(self, user: User):
         return self.__user_repository.read_by_email(user.email)
